d0cf33439487262165c30fc76db42a669ab6465e
frol4le <frol4le@gmail.com>
2020-11-08 14:24:32 +0300

    Добавление файлов

---
+++ HC-SR04/HC-SR04.ino
@@ -0,0 +1,30 @@
+const int trigPin = 15;  //D4
+const int echoPin = 0;  //D3
+
+
+long duration;
+int distance=0;
+int distance1=0;
+
+void setup() {
+    pinMode(trigPin, OUTPUT); 
+    pinMode(echoPin, INPUT); 
+    Serial.begin(115200);
+}
+
+void loop() {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    
+    duration = pulseIn(echoPin, HIGH);
+    distance= duration*0.034/2;
+    if(distance-distance1<300 && distance!=distance1 && distance!=0){
+      Serial.print("Distance: ");
+      Serial.println(distance);
+      distance1=distance;   
+    }   
+    delay(50);
+}
